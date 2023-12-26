c93f312bfee72d018403d5cc2461a3ec3f60116c
Charlemagne44 <collinhay44@gmail.com>
2023-12-26 14:11:36 -0500

    remove testing from main app file

--- src/main/java/com/charlemagne44/jweather/App.java
+++ src/main/java/com/charlemagne44/jweather/App.java
@@ -9,22 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Use Geocoding to get location lat and long from more common types
-        String loc = "Nigeria";
-        List<Double> latlong = Geocode.getLatLong(loc);
-        double latitude = latlong.get(0);
-        double longitude = latlong.get(1);
-        System.out.printf("Lat long extracted: %f %f from location: %s\n", latitude, longitude, loc);
-
-        // Get weather data
-        WeatherData data = new WeatherData(latitude, longitude);
-        Map<String,String> currentWeather = data.getCurrentWeatherData();
-        System.out.println(currentWeather);
-
-        Map<String,String> hourlyWeather = data.getHourlyWeatherData();
-        System.out.println(hourlyWeather);
-
-        Map<String,String> dailyWeather = data.getDailyWeatherData();
-        System.out.println(dailyWeather);
+        
     }
 }
