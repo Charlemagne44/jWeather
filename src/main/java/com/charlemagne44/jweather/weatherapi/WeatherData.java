3f5289b6efd04a95439c40fd07cd08c493ef9017
Charlemagne44 <collinhay44@gmail.com>
2023-12-26 13:28:25 -0500

    weather data cleanup

--- src/main/java/com/charlemagne44/jweather/weatherapi/WeatherData.java
+++ src/main/java/com/charlemagne44/jweather/weatherapi/WeatherData.java
@@ -16,28 +16,6 @@ public class WeatherData {
     double latitude;
     double longitude;
 
-    /*
-     *         String currentFields = "&current=temperature_2m,"
-                        +"relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,"
-                        +"cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,"
-                        +"wind_gusts_10m";
-        String hourlyFields = "&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,"
-                        +"precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,pressure_msl"
-                        +",surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,"
-                        +"evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_80m,"
-                        +"wind_speed_120m,wind_direction_10m,wind_direction_80m,wind_direction_120m,temperature_80m,"
-                        +"temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,"
-                        +"uv_index,uv_index_clear_sky,freezing_level_height,sunshine_duration,shortwave_radiation,direct_radiation,"
-                        +"diffuse_radiation,direct_normal_irradiance,terrestrial_radiation,shortwave_radiation_instant,"
-                        +"direct_radiation_instant,diffuse_radiation_instant,direct_normal_irradiance_instant,"
-                        +"terrestrial_radiation_instant";
-        String dailyFields = "&daily=weather_code,sunrise,sunset,daylight_duration,sunshine_duration,"
-                        +"uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,"
-                        +"precipitation_probability_max";
-     */
-
-    // Hourly forecast
-
     public double getLatitude() {
         return latitude;
     }
@@ -108,7 +86,6 @@ public class WeatherData {
     }
 
     private String getWeatherJSON(String requestFields) {
-        // String baseUrl = "https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current=temperature_2m,wind_speed_10m,relative_humidity_2m";
         String baseUrl = "https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f";
         String callUrl = String.format(baseUrl, this.getLatitude(), this.getLongitude()) + requestFields;
 
