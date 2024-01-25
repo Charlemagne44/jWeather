f67971fe68c2eaff6507deef50ad341d58347a1c
Charlemagne44 <collinhay44@gmail.com>
2024-01-25 13:08:58 -0500

    missed ignore

--- src/test/java/com/charlemagne44/jweather/apirequest/ApirequestTest.java
+++ src/test/java/com/charlemagne44/jweather/apirequest/ApirequestTest.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ApirequestTest {
     @Test
-    // @Ignore("Ignore all tests with real API calls")
+    @Ignore("Ignore automation for test with Real API calls")
     public void TestRequest() {
         String url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&timezone=auto";
         String string_response = Apirequest.request(url);
-        // check if temperature 2m is in the current response array
         try {
             JSONObject response  = new JSONObject(string_response);
             JSONObject current = response.getJSONObject("current");
